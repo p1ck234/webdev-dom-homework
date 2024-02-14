0455d072858df90ff98a9f16301cc577befd597d
p1ck23 <danil-vetrov47@mail.ru>
2024-02-14 15:58:39 +0400

    build a form for registration authorization

--- script.js
+++ script.js
@@ -16,7 +16,35 @@ const listElement = document.getElementById("list");
 const nameInputElement = document.getElementById("input-name");
 const commentInputElement = document.getElementById("comment-input");
 const deleteLastButton = document.getElementById("delete-last-button");
+const linkAuthElement = document.querySelector(".link-auth");
+const linkAuthTextElement = document.querySelector(".link-text");
+const commentsElement = document.querySelector(".comments");
+const formAuthElement = document.querySelector("#add-form-auth");
+const regLinkElement = document.querySelector("#reg-link");
+const regFormElement = document.querySelector("#add-form-reg");
+const authFormElement = document.querySelector("#link-authfromreg");
+
 let comments = [];
+
+linkAuthElement.addEventListener("click", () => {
+  commentsElement.classList.toggle("hidden");
+  linkAuthTextElement.classList.toggle("hidden");
+  formAuthElement.classList.remove("hidden");
+  formAuthElement.classList.add("auth__form");
+
+  regLinkElement.addEventListener("click", () => {
+    formAuthElement.classList.add("hidden");
+    formAuthElement.classList.remove("auth__form");
+    regFormElement.classList.remove("hidden");
+    regFormElement.classList.add("auth__form");
+  });
+  authFormElement.addEventListener("click", () => {
+    regFormElement.classList.add("hidden");
+    regFormElement.classList.remove("auth__form");
+    formAuthElement.classList.remove("hidden");
+    formAuthElement.classList.add("auth__form");
+  });
+});
 
 fetchPromiseGet(comments);
 renderComments(comments);
