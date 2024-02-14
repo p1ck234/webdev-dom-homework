c1fcfb32f2a4004a8c06b965e199e7099bc981f0
p1ck23 <danil-vetrov47@mail.ru>
2024-02-14 16:29:21 +0400

    I made the registration

--- script.js
+++ script.js
@@ -24,6 +24,14 @@ const regLinkElement = document.querySelector("#reg-link");
 const regFormElement = document.querySelector("#add-form-reg");
 const authFormElement = document.querySelector("#link-authfromreg");
 
+const regBtnElement = document.querySelector("#reg-btn");
+const regLoginElement = document.querySelector("#reg-login");
+const regNameElement = document.querySelector("#reg-name");
+const regPassElement = document.querySelector("#reg-password");
+const authBtnElement = document.querySelector("#auth-btn");
+const authLoginElement = document.getElementById("auth-login");
+const authPassElement = document.getElementById("auth-password");
+
 let comments = [];
 
 linkAuthElement.addEventListener("click", () => {
@@ -43,6 +51,47 @@ linkAuthElement.addEventListener("click", () => {
     regFormElement.classList.remove("auth__form");
     formAuthElement.classList.remove("hidden");
     formAuthElement.classList.add("auth__form");
+  });
+});
+
+regBtnElement.addEventListener("click", () => {
+  const fetchPromise = fetch("https://wedev-api.sky.pro/api/user", {
+    method: "post",
+    body: JSON.stringify({
+      login: regLoginElement.value,
+      name: regNameElement.value,
+      password: regPassElement.value,
+    }),
+  })
+    .then((response) => {
+      if (response.status === 201) {
+        return response;
+      } else if (response.status === 400) {
+        throw new Error("ошибка 400");
+      }
+    })
+    .then((responseData) => {
+      alert("Вы успешно зарегистрировались.");
+      regLoginElement.value = "";
+      regNameElement.value = "";
+      regPassElement.value = "";
+    })
+    .catch((error) => {
+      if (error.message === "ошибка 400") {
+        alert("Пользователь с такими данными уже есть, попробуйте снова");
+      } else {
+        alert("Кажется, у вас сломался интернет, попробуйте позже");
+      }
+    });
+});
+
+authBtnElement.addEventListener("click", () => {
+  const fetchPromise = fetch("https://wedev-api.sky.pro/api/user", {
+    method: "post",
+    body: JSON.stringify({
+      login: authLoginElement.value,
+      password: authPassElement.value,
+    }),
   });
 });
 
